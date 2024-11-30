e8982e20e53a813b6d2670a0e3f5d08bd8bab8b8
justin_sus1 <99390563+justinsus1@users.noreply.github.com>
2024-11-30 13:33:02 +1100

    Update config.sh

--- install/pacman/config.sh
+++ install/pacman/config.sh
@@ -1,3 +1,4 @@
-echo "ParallelDownloads = 10" >> /etc/pacman.conf
-echo "[multilib]" >> /etc/pacman.conf
-echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
+echo "[options]" | sudo tee -a /etc/pacman.conf
+echo "ParallelDownloads = 10" | sudo tee -a /etc/pacman.conf
+echo "[multilib]" | sudo tee -a /etc/pacman.conf
+echo "Include = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacman.conf
