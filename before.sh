217280b0589814f8f5687e5d56bce0983c75c797
justin_sus1 <99390563+justinsus1@users.noreply.github.com>
2024-12-01 23:17:20 +1100

    Update before.sh

--- before.sh
+++ before.sh
@@ -2,3 +2,5 @@ echo "[options]" | sudo tee -a /etc/pacman.conf
 echo "ParallelDownloads = 10" | sudo tee -a /etc/pacman.conf
 echo "[multilib]" | sudo tee -a /etc/pacman.conf
 echo "Include = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacman.conf
+
+pacman -Syu
