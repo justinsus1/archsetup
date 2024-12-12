e41bacfda53eebb19bf8cda02d8335f2ff349aa7
justinsus1 <justin_sus1@outlook.com>
2024-12-12 15:43:25 +1100

    idk

--- dotfiles/.config/Code - OSS/User/History/-36d739ba/4BP5.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
-import pyautogui
-import keyboard
-import time
-
-
--- dotfiles/.config/Code - OSS/User/History/-36d739ba/hpDT.py
+++
@@ -1,8 +0,0 @@
-import logging
-from selenium import webdriver
-
-logging.basicConfig(level=logging.DEBUG)
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-print(driver.title)
-driver.quit()
