38cc657b2a8ba09b4555a5209a38725ca9f1a414
justinsus1 <justin_sus1@outlook.com>
2025-01-26 14:04:49 +1100

    jgdndshvbdbfha

---
+++ dotfiles/.config/ml4w/scripts/yazi.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export PATH=$PATH:/home/justin_sus1/.cargo/bin
+yazi
+
--- install/arch/ollama.sh
+++ install/arch/ollama.sh
@@ -1,2 +1,3 @@
 curl -fsSL https://ollama.com/install.sh | sh
 curl -f https://zed.dev/install.sh | sh
+bash -c "$(curl -sLo- https://superfile.netlify.app/install.sh)"
